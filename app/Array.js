fdb2b0bd58892fb8f44a7c6422567ab17cd3d0b8
Olav Haugen <olahau@gmail.com>
2015-02-06 16:43:00 +0100

    Initial commit

---
+++ app/Array.js
@@ -0,0 +1,11 @@
+// Simple compare function of two arrays
+Array.prototype.equals = function(otherArray) {
+	if (this.length !== otherArray.length) {
+		return false;
+	}
+	for (var i = 0; i < this.length; i++)
+		if (this[i] !== otherArray[i]) {
+			return false;
+		}
+   return true;
+ };
---
+++ app/click.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+	'use strict';
+	var button = document.querySelector('#theButton');
+	var display = document.querySelector('#display');
+
+	var clickStream = Bacon.fromEventTarget(button, 'click');
+	var clickCountStream = clickStream
+		.bufferWithTime(300)
+		.map(function (events) {
+			return events.length;
+		});
+		
+	var singleClickStream = clickCountStream.filter(function (count) { 
+		return count === 1; 
+	});
+	var doubleClickStream = clickCountStream.filter(function (count) { 
+		return count === 2; 
+	});
+	var multiClickStream = clickCountStream.filter(function (count) { 
+		return count > 2; 
+	});
+
+	singleClickStream.onValue(function (e) {
+		display.textContent = 'You clicked once!';
+	});
+
+	doubleClickStream.onValue(function (e) {
+		display.textContent = 'You doubleclicked!';
+	});	
+
+	multiClickStream.onValue(function (e) {
+		display.textContent = 'You clicked ' + e + ' times';
+	});	
+};
